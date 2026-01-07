351a20397435d7c77ca43e330eee13e36aafe542
Calvin Irwin <cdialpha@gmail.com>
2026-01-07 16:13:12 -0500

    test commit

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C6lcMdfM.mjs';
-import { manifest } from './manifest_Bj5fmReV.mjs';
+import { manifest } from './manifest_JQuNnJiO.mjs';
 
 const serverIslandMap = new Map();;
 
