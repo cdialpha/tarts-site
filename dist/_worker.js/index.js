d3d2303404b3c3bc42423b720046831d281340aa
Calvin Irwin <cdialpha@gmail.com>
2026-01-08 13:50:17 -0500

    added donations page

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,37 +1,39 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C6lcMdfM.mjs';
-import { manifest } from './manifest_JQuNnJiO.mjs';
+import { manifest } from './manifest_CBEE_s1q.mjs';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/about.astro.mjs');
-const _page1 = () => import('./pages/fr/about.astro.mjs');
-const _page2 = () => import('./pages/fr/projects/soil.astro.mjs');
-const _page3 = () => import('./pages/fr/projects.astro.mjs');
-const _page4 = () => import('./pages/fr/resources.astro.mjs');
-const _page5 = () => import('./pages/fr.astro.mjs');
-const _page6 = () => import('./pages/projects/animals.astro.mjs');
-const _page7 = () => import('./pages/projects/community.astro.mjs');
-const _page8 = () => import('./pages/projects/foodsystems.astro.mjs');
-const _page9 = () => import('./pages/projects/soil.astro.mjs');
-const _page10 = () => import('./pages/projects.astro.mjs');
-const _page11 = () => import('./pages/resources.astro.mjs');
-const _page12 = () => import('./pages/index.astro.mjs');
+const _page1 = () => import('./pages/donate.astro.mjs');
+const _page2 = () => import('./pages/fr/about.astro.mjs');
+const _page3 = () => import('./pages/fr/projects/soil.astro.mjs');
+const _page4 = () => import('./pages/fr/projects.astro.mjs');
+const _page5 = () => import('./pages/fr/resources.astro.mjs');
+const _page6 = () => import('./pages/fr.astro.mjs');
+const _page7 = () => import('./pages/projects/animals.astro.mjs');
+const _page8 = () => import('./pages/projects/community.astro.mjs');
+const _page9 = () => import('./pages/projects/foodsystems.astro.mjs');
+const _page10 = () => import('./pages/projects/soil.astro.mjs');
+const _page11 = () => import('./pages/projects.astro.mjs');
+const _page12 = () => import('./pages/resources.astro.mjs');
+const _page13 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["src/pages/about.astro", _page0],
-    ["src/pages/fr/about.astro", _page1],
-    ["src/pages/fr/projects/soil.astro", _page2],
-    ["src/pages/fr/projects.astro", _page3],
-    ["src/pages/fr/resources.astro", _page4],
-    ["src/pages/fr/index.astro", _page5],
-    ["src/pages/projects/animals.astro", _page6],
-    ["src/pages/projects/community.astro", _page7],
-    ["src/pages/projects/foodsystems.astro", _page8],
-    ["src/pages/projects/soil.astro", _page9],
-    ["src/pages/projects.astro", _page10],
-    ["src/pages/resources.astro", _page11],
-    ["src/pages/index.astro", _page12]
+    ["src/pages/donate.astro", _page1],
+    ["src/pages/fr/about.astro", _page2],
+    ["src/pages/fr/projects/soil.astro", _page3],
+    ["src/pages/fr/projects.astro", _page4],
+    ["src/pages/fr/resources.astro", _page5],
+    ["src/pages/fr/index.astro", _page6],
+    ["src/pages/projects/animals.astro", _page7],
+    ["src/pages/projects/community.astro", _page8],
+    ["src/pages/projects/foodsystems.astro", _page9],
+    ["src/pages/projects/soil.astro", _page10],
+    ["src/pages/projects.astro", _page11],
+    ["src/pages/resources.astro", _page12],
+    ["src/pages/index.astro", _page13]
 ]);
 
 const _manifest = Object.assign(manifest, {
