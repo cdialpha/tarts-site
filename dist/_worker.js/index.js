8f3bff02d1707e38a17c9fc58914277e0d30875e
Calvin Irwin <cdialpha@gmail.com>
2026-01-12 17:57:16 -0500

    added email newsletter modal

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C6lcMdfM.mjs';
-import { manifest } from './manifest_CBEE_s1q.mjs';
+import { manifest } from './manifest_C84hda4x.mjs';
 
 const serverIslandMap = new Map();;
 
