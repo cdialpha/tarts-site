555d8b323d3409f1c52b4e13c67ec99067e6eb7d
Calvin Irwin <cdialpha@gmail.com>
2026-02-26 16:15:24 -0500

    fixed build issue w/ image

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_IgC5kHL1.mjs';
-import { manifest } from './manifest_DXv-OyHh.mjs';
+import { manifest } from './manifest_DNCOEAPW.mjs';
 
 const serverIslandMap = new Map();;
 
